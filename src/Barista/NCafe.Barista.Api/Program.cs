d5902c21d63b41e3ce36ec0b9c73e99645be0963
Fredi Machado <fredisoft@gmail.com>
2022-01-10 20:48:49 +1100

    Add Barista projects

---
+++ src/Barista/NCafe.Barista.Api/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapPost("/orders/{id:guid}/prepared", () =>
+{
+    
+})
+.WithName("CompletePreparation");
+
+app.Run();
