e47d4e1fa1a048316ab460d8a7716c04ba2cf8f2
Fredi Machado <fredisoft@gmail.com>
2022-01-10 13:44:29 +1100

    Add Product aggregate and ProductCreated event to Admin

---
+++ src/Admin/NCafe.Admin.Domain/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.GuardClauses;
+using NCafe.Abstractions.Domain;
+using NCafe.Admin.Domain.Events;
+
+namespace NCafe.Admin.Domain.Entities;
+
+public sealed class Product : AggregateRoot<Guid>
+{
+    private Product()
+    {
+    }
+
+    public Product(Guid id, string name, decimal price)
+    {
+        Id = Guard.Against.Default(id, nameof(id));
+        Name = Guard.Against.NullOrWhiteSpace(name, nameof(name));
+        Price = Guard.Against.NegativeOrZero(price, nameof(price));
+
+        RaiseEvent(new ProductCreated(id, name, price));
+    }
+
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+
+    public void Apply(ProductCreated @event)
+    {
+        Id = @event.Id;
+        Name = @event.Name;
+        Price = @event.Price;
+    }
+}
---
+++ src/Admin/NCafe.Admin.Domain/Events/ProductCreated.cs
@@ -0,0 +1,16 @@
+﻿using NCafe.Abstractions.Domain;
+
+namespace NCafe.Admin.Domain.Events;
+
+public sealed class ProductCreated : Event
+{
+    public ProductCreated(Guid id, string name, decimal price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+
+    public string Name { get; }
+    public decimal Price { get; }
+}
