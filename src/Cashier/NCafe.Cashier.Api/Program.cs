4822f9d6fadb21332c91cb832ae57292c5cf8bb3
Fredi Machado <fredisoft@gmail.com>
2022-01-10 20:23:30 +1100

    Add endpoint to get all products from Cashier's read model

--- src/Cashier/NCafe.Cashier.Api/Program.cs
+++ src/Cashier/NCafe.Cashier.Api/Program.cs
@@ -1,7 +1,9 @@
 using NCafe.Abstractions.Commands;
+using NCafe.Abstractions.Queries;
 using NCafe.Abstractions.ReadModels;
 using NCafe.Cashier.Api.ReadModel;
 using NCafe.Cashier.Application.Commands;
+using NCafe.Cashier.Application.Queries;
 using NCafe.Cashier.Application.ReadModels;
 using NCafe.Infrastructure;
 using NCafe.Infrastructure.ReadModels;
@@ -11,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddEventStoreRepository(builder.Configuration);
 builder.Services.AddCommandHandlers(typeof(PlaceOrder).Assembly);
+builder.Services.AddQueryHandlers(typeof(PlaceOrder).Assembly);
 
 builder.Services.AddSingleton<IReadModelRepository<Product>, InMemoryReadModelRepository<Product>>();
 builder.Services.AddHostedService<Worker>();
@@ -28,6 +31,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/products", async (IQueryDispatcher queryDispatcher) =>
+{
+    var result = await queryDispatcher.QueryAsync(new GetProducts());
+    return Results.Ok(result);
+})
+.WithName("GetProducts");
 
 app.MapPost("/order", async (ICommandDispatcher commandDispatcher, PlaceOrder command) =>
 {
