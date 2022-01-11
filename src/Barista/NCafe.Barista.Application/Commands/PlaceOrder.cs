36af40c980049cd335bf5e419b50ec74418cb07d
Fredi Machado <fredisoft@gmail.com>
2022-01-11 19:39:04 +1100

    Add PlaceOrder command and handler to Barista project
    This command will be executed by the event bus consumer

---
+++ src/Barista/NCafe.Barista.Application/Commands/PlaceOrder.cs
@@ -0,0 +1,24 @@
+﻿using NCafe.Abstractions.Commands;
+using NCafe.Abstractions.Repositories;
+using NCafe.Barista.Domain.Entities;
+
+namespace NCafe.Barista.Application.Commands;
+
+public record PlaceOrder(Guid Id, Guid ProductId, int Quantity) : ICommand;
+
+internal sealed class PlaceOrderHandler : ICommandHandler<PlaceOrder>
+{
+    private readonly IRepository repository;
+
+    public PlaceOrderHandler(IRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task HandleAsync(PlaceOrder command)
+    {
+        var order = new BaristaOrder(command.Id, command.ProductId, command.Quantity);
+
+        await repository.Save(order);
+    }
+}
---
+++ src/NCafe.Barista.Application.Tests/Commands/PlaceOrderTests.cs
@@ -0,0 +1,44 @@
+﻿using FakeItEasy;
+using NCafe.Abstractions.EventBus;
+using NCafe.Abstractions.EventBus.Events;
+using NCafe.Abstractions.ReadModels;
+using NCafe.Abstractions.Repositories;
+using NCafe.Barista.Application.Commands;
+using NCafe.Barista.Domain.Entities;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NCafe.Barista.Application.Tests.Commands;
+
+public class PlaceOrderTests
+{
+    private readonly PlaceOrderHandler sut;
+
+    private readonly IRepository repository;
+
+    public PlaceOrderTests()
+    {
+        repository = A.Fake<IRepository>();
+
+        sut = new PlaceOrderHandler(repository);
+    }
+
+    [Fact]
+    public async Task ShouldSaveOrder()
+    {
+        // Arrange
+        var orderId = Guid.NewGuid();
+        var productId = Guid.NewGuid();
+        var command = new PlaceOrder(orderId, productId, 1);
+
+        // Act
+        var exception = await Record.ExceptionAsync(() => sut.HandleAsync(command));
+
+        // Assert
+        exception.ShouldBeNull();
+        A.CallTo(() => repository.Save(A<BaristaOrder>.That.Matches(o => o.Id == orderId && o.ProductId == productId && o.Quantity == 1)))
+            .MustHaveHappenedOnceExactly();
+    }
+}
