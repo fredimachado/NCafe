9ead7908332e225623b0beed2b6c99eb5c38f965
Fredi Machado <fredisoft@gmail.com>
2022-01-12 16:17:09 +1100

    Create product projection service in Admin
    So we can have a read model for products

---
+++ src/Admin/NCafe.Admin.Api/Projections/ProductProjectionService.cs
@@ -0,0 +1,70 @@
+﻿using EventStore.Client;
+using NCafe.Abstractions.ReadModels;
+using NCafe.Admin.Domain.Events;
+using NCafe.Admin.Domain.ReadModels;
+using System.Text.Json;
+
+namespace NCafe.Admin.Api.Projections;
+
+public class ProductProjectionService : BackgroundService
+{
+    private readonly IServiceProvider serviceProvider;
+    private readonly EventStoreClient eventStoreClient;
+    private readonly IReadModelRepository<Product> productReadRepository;
+    private readonly ILogger logger;
+
+    public ProductProjectionService(
+        IServiceProvider serviceProvider,
+        EventStoreClient eventStoreClient,
+        IReadModelRepository<Product> productReadRepository,
+        ILogger<ProductProjectionService> logger)
+    {
+        this.serviceProvider = serviceProvider;
+        this.eventStoreClient = eventStoreClient;
+        this.productReadRepository = productReadRepository;
+        this.logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        await eventStoreClient.SubscribeToStreamAsync(
+            "$ce-product",
+            ProductEventAppeared,
+            subscriptionDropped: SubscriptionDropped,
+            resolveLinkTos: true,
+            cancellationToken: stoppingToken);
+
+        logger.LogInformation("Subscribed to EventStore Stream.", DateTimeOffset.Now);
+
+        await Task.Delay(Timeout.Infinite, stoppingToken);
+    }
+
+    private Task ProductEventAppeared(StreamSubscription subscription, ResolvedEvent @event, CancellationToken cancellationToken)
+    {
+        if (@event.Event.EventType == nameof(ProductCreated))
+        {
+            var state = JsonSerializer.Deserialize<ProductCreated>(@event.Event.Data.Span);
+            var product = new Product()
+            {
+                Id = state.Id,
+                Name = state.Name,
+                Price = state.Price
+            };
+            productReadRepository.Add(product);
+            logger.LogInformation(
+                "Added product '{productName}' ({productId}) with price ${productPrice}",
+                product.Name,
+                product.Id,
+                product.Price);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private void SubscriptionDropped(StreamSubscription arg1, SubscriptionDroppedReason arg2, Exception arg3)
+    {
+        logger.LogError("Subscription Dropped.", DateTimeOffset.Now);
+    }
+}
---
+++ src/Admin/NCafe.Admin.Domain/ReadModels/Product.cs
@@ -0,0 +1,9 @@
+﻿using NCafe.Abstractions.ReadModels;
+
+namespace NCafe.Admin.Domain.ReadModels;
+
+public sealed class Product : ReadModel
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
