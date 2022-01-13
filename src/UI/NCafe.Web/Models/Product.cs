187b484a75360b4819077c7fde2df7774249ec02
Fredi Machado <fredisoft@gmail.com>
2022-01-13 19:12:10 +1100

    Use AntBlazor in Admin pages

--- src/UI/NCafe.Web/Models/Product.cs
+++ src/UI/NCafe.Web/Models/Product.cs
@@ -1,8 +1,10 @@
-﻿namespace NCafe.Web.Pages.Admin;
+﻿namespace NCafe.Web.Models;
 
 public class Product
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
+
+    public override string ToString() => Name;
 }
---
+++ src/UI/NCafe.Web/Models/Products.cs
@@ -0,0 +1,6 @@
+﻿namespace NCafe.Web.Models;
+
+public sealed class Products
+{
+    public IEnumerable<Product> Items { get; set; } = Enumerable.Empty<Product>();
+}
