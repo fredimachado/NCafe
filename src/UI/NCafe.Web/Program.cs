c134d47353074d3bfadd5cf162025100da2747e2
Fredi Machado <fredisoft@gmail.com>
2022-01-13 19:09:03 +1100

    Add AntBlazor
    https://antblazor.com/

--- src/UI/NCafe.Web/Program.cs
+++ src/UI/NCafe.Web/Program.cs
@@ -8,4 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddAntDesign();
+
 await builder.Build().RunAsync();
