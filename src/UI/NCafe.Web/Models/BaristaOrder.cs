2f1c0f3611a8152cb12a798e0acfae87518a8d92
Fredi Machado <fredisoft@gmail.com>
2022-01-14 15:53:32 +1100

    Show new orders in Barista page and enable completing orders

---
+++ src/UI/NCafe.Web/Models/BaristaOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace NCafe.Web.Models;
+
+public class BaristaOrder
+{
+    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+    public bool IsCompleted { get; set; }
+}
