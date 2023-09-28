88cfa2ad73669b75ceea0c4e22e7caf95743d565
Fredi Machado <fredisoft@gmail.com>
2023-09-28 17:57:32 +1000

    Use script to  replace base addresses on startup

---
+++ src/UI/NCafe.Web/prepare-appsettings.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+APPSETTINGS=/user/share/nginx/html/appsettings.json
+
+# delete compressed (brotli/gzip) appsettings files
+rm "$APPSETTINGS.br"
+rm "$APPSETTINGS.gz"
+
+# update base address value using environment variables
+sed -i -E "s|(\"AdminBaseAddress\"\:) \"(.*)\"(,)?|\1 \"$ADMIN_BASE_ADDRESS\"\3|g" $APPSETTINGS
+sed -i -E "s|(\"CashierBaseAddress\"\:) \"(.*)\"(,)?|\1 \"$CASHIER_BASE_ADDRESS\"\3|g" $APPSETTINGS
+sed -i -E "s|(\"BaristaBaseAddress\"\:) \"(.*)\"(,)?|\1 \"$BARISTA_BASE_ADDRESS\"\3|g" $APPSETTINGS
+
+cat $APPSETTINGS
