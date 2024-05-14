2eb76f84facf87063ca4ad937cb587a78bd7b841
Fredi Machado <fredisoft@gmail.com>
2024-05-14 19:43:51 +1000

    Add architectural tests

---
+++ src/Common/NCafe.Common.Tests/Architecture/InfrastructureTests.cs
@@ -0,0 +1,35 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using ArchUnitNET.xUnit;
+using ArchUnitNET.Fluent;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace NCafe.Common.Tests.Architecture;
+public class InfrastructureTests
+{
+    private readonly static System.Reflection.Assembly InfrastructureAssembly =
+        System.Reflection.Assembly.Load("NCafe.Infrastructure");
+
+    private readonly static ArchUnitNET.Domain.Architecture Architecture =
+        new ArchLoader().LoadAssemblies(
+            InfrastructureAssembly
+        ).Build();
+
+    private readonly IObjectProvider<IType> CommonInfrastructure =
+        Types().That().ResideInAssembly(InfrastructureAssembly)
+        .As("Infrastructure Layer");
+
+    private readonly IObjectProvider<IType> DependencyRegistrationClass =
+        Types().That().HaveName("DependencyRegistration")
+        .As("DependencyRegistration class");
+
+    [Fact]
+    public void Infrastructure_Project_Types_Should_Be_Internal()
+    {
+        IArchRule typesShouldBeInternal = Types().That().Are(CommonInfrastructure)
+            .And().AreNot(DependencyRegistrationClass).Should().BeInternal()
+            .Because("Other layers should not depend on implementation details, only interfaces.");
+
+        typesShouldBeInternal.Check(Architecture);
+    }
+}
