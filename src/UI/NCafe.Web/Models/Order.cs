3138c4e745f4f0f9396647446de46b754fb89aec
Andre Foresti <Andre.Foresti@purple.telstra.com>
2024-05-25 16:43:38 +1000

    Cashier UI remodel
    - Add support to use tailwind;
    - Show all available products as buttons in a grid
    - Add item to order when the product button is clicked
    - Create the order on the first item
    - Show summary of the order on the left side
    - Asks for customer name in a modal
    - Place the order when confirm the modal

--- src/UI/NCafe.Web/Models/Order.cs
+++ src/UI/NCafe.Web/Models/Order.cs
@@ -1,7 +1,23 @@
 ﻿namespace NCafe.Web.Models;
 
+public class Order
+{
+    public Guid Id { get; set; }
+    public string CustomerName { get; set; }
+    public List<OrderItem> Items { get; set; } = new();
+    public decimal Total => Items.Sum(x => x.Total);
+    public void Clear()
+    {
+        Items.Clear();
+        Id = Guid.Empty;
+        CustomerName = string.Empty;
+    }
+}
+
 public class OrderItem
 {
     public Guid ProductId { get; set; }
     public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public decimal Total => Quantity * Price;
 }
